e6053b4088c588a7f39323992e2f8d226cd1e4f4
knangcas <knangcas@asu.edu>
2025-01-27 23:49:51 -0600

    junit testing doesn't work

--- src/test/java/VictoryTest.java
+++ src/test/java/VictoryTest.java
@@ -1,6 +1,8 @@
 import knangcas.connectFour.exception.ColumnFullException;
 import knangcas.connectFour.model.ConnectBoard;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
