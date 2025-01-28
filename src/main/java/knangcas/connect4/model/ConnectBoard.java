372c67842290e5b4a5de87914b598b0fca302787
knangcas <knangcas@asu.edu>
2025-01-27 18:04:29 -0600

    added logic for vertical checking, working on horizontal

--- src/main/java/knangcas/connect4/model/ConnectBoard.java
+++ src/main/java/knangcas/connect4/model/ConnectBoard.java
@@ -13,6 +13,8 @@ public class ConnectBoard {
     HashMap<Integer, Spot> boardHash;
     List<Stack<Integer>> columns;
 
+    private int lastSpot;
+
 
     private int playerTurn;
     public ConnectBoard() {
@@ -35,11 +37,56 @@ public class ConnectBoard {
         if (columns.get(colNumber-1).size() == 6) {
             throw new ColumnFullException("Current column is full");
         }
-
+        lastSpot = spotConversion(colNumber-1, columns.get(colNumber-1).size());
         columns.get(colNumber-1).push(playerNumber);
-        boardHash.get(spotConversion(colNumber-1, columns.get(colNumber-1).size())).setIsOccupied(playerNumber);
+        boardHash.get(lastSpot).setIsOccupied(playerNumber);
+
+        checkVictory();
+        playerTurnChange();
 
 
+    }
+
+    private int getRowFromSpot(int coordinate) {
+        if (coordinate % 6 > 0) {
+            return (coordinate / 6);
+        }
+        return (coordinate / 6) - 1;
+    }
+
+    private boolean checkVictory() {
+        //upper difference = 5;
+        //lower difference = 7;
+
+        //check vertically
+        if (lastSpot - (getRowFromSpot(lastSpot) * 6) <=3) {
+            checkVertically();
+        }
+
+        //check horizontally;
+
+
+
+        return false;
+
+    }
+
+    private boolean checkVertically() {
+        int result = 0;
+        for (int i = lastSpot+1; i < lastSpot+4; i++) {
+            if (boardHash.get(i).getIsOccupied() == playerTurn) {
+                result++;
+            }
+        }
+        return result == 4;
+    }
+
+    private void playerTurnChange() {
+        if (playerTurn == 1) {
+            playerTurn = 2;
+        } else {
+            playerTurn = 1;
+        }
     }
 
     private int spotConversion(int col, int spot) {
